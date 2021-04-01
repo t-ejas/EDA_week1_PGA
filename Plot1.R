9c26c511d7f15f787831feeee256beaf070a111e
t-ejas <81732897+t-ejas@users.noreply.github.com>
2021-04-01 20:15:10 +0530

    Update Plot1.R

--- Plot1.R
+++ Plot1.R
@@ -1,6 +1,6 @@
 dataset<-read.table("household_power_consumption.txt", header=TRUE,skip=1,sep=";")
 dt1<-dataset[dataset$Date=="1/2/2007",]
-dt2=dataset[dataset$Date=="2/2/2007",]
+dt2<-dataset[dataset$Date=="2/2/2007",]
 data_pwr <- rbind(dt1,dt2)
 data_pwr$Global_active_power<-as.numeric(data_date$Global_active_power)
 hist(data_pwr$Global_active_power)
